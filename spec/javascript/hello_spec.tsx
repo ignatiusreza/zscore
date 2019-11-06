c45f125379e7eb95a6e301a5091dcff6ee7f9487
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-11-06 11:21:35 +0900

    setup jest to cover frontend test

---
+++ spec/javascript/hello_spec.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Hello from "hello";
+
+describe("Hello", () => {
+  it("works for testing react component", () => {
+    expect(<Hello />).toMatchSnapshot();
+  });
+});
