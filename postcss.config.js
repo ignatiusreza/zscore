78fd5d643e8d190e690f45c96d36b8c2202d1d32
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-11-07 23:26:01 +0900

    cleanup unused css via purgecss
    css bundle size:
    
    - before: 67.6 KB
    - after: 1.6 KB

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
+
+const src = path.resolve(__dirname, 'app/javascript/**/*.{js,jsx,ts,tsx}');
+const views = path.resolve(__dirname, 'app/views/**/*.html.erb');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [src, views],
+
+  // custom extractor to match tailwind classes
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
   plugins: [
     require('postcss-import'),
@@ -9,5 +20,6 @@ module.exports = {
       stage: 3,
     }),
     require('tailwindcss'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
