26dd8b1f660edfb0bc1e15d0d5a3c8ecbed7d841
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-11-07 14:18:33 +0900

    configure jest to understand css files
    just noticed that the sample hello spec is not
    taking the right snapshot.. :facepalm:

--- spec/javascript/hello_spec.tsx
+++ spec/javascript/hello_spec.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Hello from 'hello';
+import renderer from 'react-test-renderer';
 
 describe('Hello', () => {
   it('works for testing react component', () => {
-    expect(<Hello />).toMatchSnapshot();
+    const output = renderer.create(<Hello />).toJSON();
+
+    expect(output).toMatchSnapshot();
   });
 });
