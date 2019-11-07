c6a8f439d55202d22c64d5a093373455120a1da8
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-11-07 12:24:58 +0900

    setup css-modules

--- app/javascript/hello.tsx
+++ app/javascript/hello.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import styles from './hello.module.css';
+
 interface HelloProps {
   name?: string;
 }
 
-const Hello: React.FC<HelloProps> = props => <div>Hello {props.name}!</div>;
+const Hello: React.FC<HelloProps> = props => <div className={styles.hello}>Hello {props.name}!</div>;
 
 Hello.defaultProps = {
   name: 'David',
--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -15,3 +15,9 @@ require('channels');
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from 'hello';
+
+ReactDOM.render(<Hello />, document.getElementById('react-root'));
